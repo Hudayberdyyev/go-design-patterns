7da307739a4bf750eaef497dabdc5624b0002541
hudayberdyyev <hudayberdyyev.ahmet@gmail.com>
2023-02-24 17:52:23 +0500

    factory method, upgrading debit card method

--- creational/factory/factory.go
+++ creational/factory/factory.go
@@ -20,12 +20,15 @@ type CashPM struct {
 type DebitCardPM struct {
 }
 
+type CreditCardPM struct {
+}
+
 func GetPaymentMethod(m int) (PaymentMethod, error) {
 	switch m {
 	case Cash:
 		return new(CashPM), nil
 	case DebitCard:
-		return new(DebitCardPM), nil
+		return new(CreditCardPM), nil
 	default:
 		return nil, errors.New(fmt.Sprintf("payment method %d not recognized", m))
 	}
@@ -35,6 +38,10 @@ func (c *CashPM) Pay(amount float32) string {
 	return fmt.Sprintf("%0.2f paid using cash\n", amount)
 }
 
-func (c *DebitCardPM) Pay(amount float32) string {
+func (d *DebitCardPM) Pay(amount float32) string {
 	return fmt.Sprintf("%#0.2f paid using debit card\n", amount)
+}
+
+func (c *CreditCardPM) Pay(amount float32) string {
+	return fmt.Sprintf("%0.2f paid using debit card (new)\n", amount)
 }
